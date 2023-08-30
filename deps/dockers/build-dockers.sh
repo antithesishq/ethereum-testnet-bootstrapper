6d4ae48613dc12afc417ec1a5c79f0b58e8aa1fe
Stefan <stefan@starflinger.eu>
2023-08-30 12:16:14 +0200

    build the mainnet current images

--- deps/dockers/build-dockers.sh
+++ deps/dockers/build-dockers.sh
@@ -23,14 +23,14 @@ function container_builder() {
     fi
 }
 
-# log_step "building minimal-current"
-# build_image "etb-all-clients:minimal-current" "etb-all-clients_minimal-current.Dockerfile"
+log_step "building minimal-current"
+build_image "etb-all-clients:minimal-current" "etb-all-clients_minimal-current.Dockerfile"
 
-# log_step "building mainnet-current"
-# build_image "etb-all-clients:mainnet-current" "etb-all-clients_mainnet-current.Dockerfile"
+log_step "building mainnet-current"
+build_image "etb-all-clients:mainnet-current" "etb-all-clients_mainnet-current.Dockerfile"
 
-log_step "building mainnet-deneb"
-build_image "etb-all-clients:deneb-devnet-7" "etb-all-clients_mainnet-deneb.Dockerfile"
+# log_step "building mainnet-deneb"
+# build_image "etb-all-clients:deneb-devnet-7" "etb-all-clients_mainnet-deneb.Dockerfile"
 
 # log_step "building mainnet-deneb-devnet-7"
 # build_image "etb-all-clients:deneb-devnet-7" "etb-all-clients_mainnet-deneb-devnet-7.Dockerfile"
