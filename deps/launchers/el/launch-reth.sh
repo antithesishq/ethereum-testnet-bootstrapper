caf5c8086914e259f52f3fb4c8ef7589768e68a2
Stefan <st3f4n.s@googlemail.com>
2023-11-06 13:49:17 +0100

    update reth launch

--- deps/launchers/el/launch-reth.sh
+++ deps/launchers/el/launch-reth.sh
@@ -17,30 +17,30 @@ if [ "$RUN_JSON_RPC_SNOOPER" == "true" ]; then
   json_rpc_snoop -p "$CL_EXECUTION_ENGINE_HTTP_PORT" http://localhost:"$EXECUTION_ENGINE_HTTP_PORT" 2>&1 | tee "$EXECUTION_NODE_DIR/json_rpc_snoop.log" &
 fi
 
-EXECUTION_CMD="reth"
-EXECUTION_CMD+=" node"
-EXECUTION_CMD+=" -$EXECUTION_LOG_LEVEL"
-EXECUTION_CMD+=" --log.directory=$EXECUTION_NODE_DIR/logs/"
-EXECUTION_CMD+=" --datadir=$EXECUTION_NODE_DIR"
-EXECUTION_CMD+=" --chain=$EXECUTION_GENESIS_FILE"
-EXECUTION_CMD+=" --port=$EXECUTION_P2P_PORT"
-EXECUTION_CMD+=" --discovery.port=$EXECUTION_P2P_PORT"
-EXECUTION_CMD+=" --http"
-EXECUTION_CMD+=" --http.api=$EXECUTION_HTTP_APIS"
-EXECUTION_CMD+=" --http.addr=0.0.0.0"
-EXECUTION_CMD+=" --http.port=$EXECUTION_HTTP_PORT"
-EXECUTION_CMD+=" --http.corsdomain=*"
-EXECUTION_CMD+=" --ws"
-EXECUTION_CMD+=" --ws.api=$EXECUTION_WS_APIS"
-EXECUTION_CMD+=" --ws.addr=0.0.0.0"
-EXECUTION_CMD+=" --ws.port=$EXECUTION_WS_PORT"
-EXECUTION_CMD+=" --ws.origins=*"
-EXECUTION_CMD+=" --nat=extip:$IP_ADDRESS"
-EXECUTION_CMD+=" --authrpc.addr=0.0.0.0"
-EXECUTION_CMD+=" --authrpc.port=$EXECUTION_ENGINE_HTTP_PORT"
-EXECUTION_CMD+=" --authrpc.jwtsecret=$JWT_SECRET_FILE"
-EXECUTION_CMD+=" --metrics=0.0.0.0:$EXECUTION_METRIC_PORT"
+reth_args=(
+  node
+  -$EXECUTION_LOG_LEVEL
+  --log.directory=$EXECUTION_NODE_DIR/logs/
+  --datadir=$EXECUTION_NODE_DIR
+  --chain=$EXECUTION_GENESIS_FILE
+  --port=$EXECUTION_P2P_PORT
+  --discovery.port=$EXECUTION_P2P_PORT
+  --http
+  --http.api=$EXECUTION_HTTP_APIS
+  --http.addr=0.0.0.0
+  --http.port=$EXECUTION_HTTP_PORT
+  --http.corsdomain=*
+  --ws
+  --ws.api=$EXECUTION_WS_APIS
+  --ws.addr=0.0.0.0
+  --ws.port=$EXECUTION_WS_PORT
+  --ws.origins=*
+  --nat=extip:$IP_ADDRESS
+  --authrpc.addr=0.0.0.0
+  --authrpc.port=$EXECUTION_ENGINE_HTTP_PORT
+  --authrpc.jwtsecret=$JWT_SECRET_FILE
+  --metrics=0.0.0.0:$EXECUTION_METRIC_PORT
+)
 
 echo "Launching reth execution client."
-eval "$EXECUTION_CMD"
-
+reth "${reth_args[@]}" > /data/logs/"service_$CONTAINER_NAME--reth" 2>&1
