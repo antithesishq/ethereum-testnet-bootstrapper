5e9b394301724076949345c6468403b00f37fb4a
Stefan <stefan@starflinger.eu>
2024-01-11 14:58:52 +0100

    add dencun goerli

---
+++ builds/dencun-goerli/build.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+# Usage:
+# $1: Image tag
+# $2: Dockerfile
+function build_image() {
+    if [ "${REBUILD_IMAGES}" == 1 ]; then
+        container_builder build --no-cache -t $1 -f $2 || echo "failed to rebuild $1"
+    else
+        container_builder build -t $1 -f $2 || echo "failed to build $1"
+    fi
+}
+# Usage:
+# $1: logging string.
+function log_step() {
+    echo "[ethereum-testnet-bootstrapper] — $1"
+}
+
+function container_builder() {
+    if hash docker 2>/dev/null; then
+        BUILDKIT=1 docker "$@" .
+    else
+        podman "$@"
+    fi
+}
+
+TAG="dencun-goerli"
+
+log_step "building all clients"
+build_image "etb-all-clients:$TAG" "etb-all-clients_mainnet.Dockerfile"
+
+log_step "building all clients instrumented"
+build_image "etb-all-clients-inst:$TAG" "etb-all-clients_mainnet-inst.Dockerfile"
+
+log_step "building all clients race"
+build_image "etb-all-clients-race:$TAG" "etb-all-clients_mainnet-race.Dockerfile"
