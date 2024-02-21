1727c5c69e10180fb9b31929b4460ab871030acc
Stefan <stefan@starflinger.eu>
2024-02-21 13:29:29 -0500

    fix

--- src/testnet_bootstrapper.py
+++ src/testnet_bootstrapper.py
@@ -552,10 +552,10 @@ class EthereumTestnetBootstrapper:
                     Path(consensus_node_dir / "validatordb").mkdir()
                 if cl_client == "grandine":
                         for key in os.listdir(keystore_src):
-                            shutil.copytree(Path(f"{keystore_src}/{key}/voting-keystore.json"), Path(f"{keystore_dst}/{key}.json"))
+                            shutil.copyfile(Path(f"{keystore_src}/{key}/voting-keystore.json"), Path(f"{keystore_dst}/{key}.json"))
                         
                         for secrets in os.listdir(secret_src):
-                            shutil.copytree(Path(f"{secret_src}/{secrets}"), Path(f"{secret_dst}/{secrets}.txt"))
+                            shutil.copyfile(Path(f"{secret_src}/{secrets}"), Path(f"{secret_dst}/{secrets}.txt"))
                 else:
                 # copy everything over
                     shutil.copytree(keystore_src, keystore_dst)
